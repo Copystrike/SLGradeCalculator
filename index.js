7fc9bc572f646283041de670132cada16c2717e7
Copystrike <verbraeckennick@gmail.com>
2021-02-19 20:49:32 +0100

    Hhmmm

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-document.body.style.border = "5px solid border";
+console.log('SUp');
+boolean isGridActive = false;
+
+$(document).arrive(".smscMainBlockContainer", function() {
+	const smscContainer = $(this);
+	console.log('Hey');
+
+	tabsClickMonitor();
+});
+
+
+function tabsClickMonitor() {
+	let tabs = $('.smscTabs').children();
+	for (let i = 0; i < tabs.length; i++) {
+		const tab = $(tabs[i]);
+		if (tabs[0].id === "eval_grid_tab") {
+			tab.click(function () {
+				isGridActive = true;
+			});
+		} else {
+			tab.click(function () {
+				isGridActive = false;
+			});
+		}
+
+		console.log(`The tab is ${isGridActive}`);
+	}
+}
