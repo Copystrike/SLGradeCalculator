a04407fc3c25b34f8bceccd96b510d23675a0689
Copystrike <verbraeckennick@gmail.com>
2021-02-20 00:21:32 +0100

    Okay its broken but will fix soon

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-console.log('SUp');
-boolean isGridActive = false;
+console.log('LOL');
+let isGridActive = false;
+let isSideBarGridActive = false;
 
 
-$(document).arrive(".smscMainBlockContainer", function() {
-	$(this).remove();
-})
-
-$(document).arrive(".smscMainBlockContainer", function() {
+$(document).arrive('.smscMainBlockContainer', function() {
 	const smscContainer = $(this);
 	console.log('Hey');
 
@@ -14,11 +11,29 @@ $(document).arrive(".smscMainBlockContainer", function() {
 });
 
 
+function initializeSideBar() {
+	let sidebar = $('.smscToolbar').children();
+	for (int i = 0; i < sidebar.length; i++) {
+		let sidebarElem = $(sidebar[i]);
+		if (sidebarElem.hasClass('icon_evals')) {
+			sidebarElem.click(function() {
+				isSideBarGridActive = true;
+			});
+		} else {
+			sidebarElem.click(function() {
+				isSideBarGridActive = false;
+			});
+		}
+	}
+}
+
+
 function tabsClickMonitor() {
 	let tabs = $('.smscTabs').children();
+	console.log(tabs);
 	for (let i = 0; i < tabs.length; i++) {
 		const tab = $(tabs[i]);
-		if (tabs[0].id === "eval_grid_tab") {
+		if (tabs[i].id === 'eval_grid_tab') {
 			tab.click(function () {
 				isGridActive = true;
 			});
